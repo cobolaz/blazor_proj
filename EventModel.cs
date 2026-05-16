d8c26cf52217bd151f8502bdb7a462d453810f34
cobolaz <109870484+cobolaz@users.noreply.github.com>
2026-05-16 20:58:41 +0000

    Initial work on Blazor

---
+++ EventModel.cs
@@ -0,0 +1,10 @@
+namespace EventEase.Models
+{
+    public class EventModel
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public DateTime Date { get; set; }
+        public string Location { get; set; } = "";
+    }
+}
