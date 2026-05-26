213bdf5a906b73c058370c6f9d25f7996468baac
cobolaz <109870484+cobolaz@users.noreply.github.com>
2026-05-26 07:25:47 -0700

    Add validation attributes to EventModel properties

--- EventModel.cs
+++ EventModel.cs
@@ -2,9 +2,17 @@ namespace EventEase.Models
 {
     public class EventModel{
         public int Id { get; set; }
+        
+        [Required, MinLength(3)]
         public string Title { get; set; } = "";
+
+        [Required]
         public string Description { get; set; } = "";
+
+        [Required]
         public DateTime Date { get; set; } = DateTime.Today;
+
+        [Required]
         public string Location { get; set; } = "";
     }
 }
