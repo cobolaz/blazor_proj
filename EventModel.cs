319dc9f4c472f1e5d4e594ed6c35d60698dc837c
cobolaz <109870484+cobolaz@users.noreply.github.com>
2026-05-25 20:57:40 -0700

    Refactor EventModel properties and default values

--- EventModel.cs
+++ EventModel.cs
@@ -1,10 +1,10 @@
 namespace EventEase.Models
 {
-    public class EventModel
-    {
+    public class EventModel{
+        public int Id { get; set; }
         public string Title { get; set; } = "";
         public string Description { get; set; } = "";
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.Today;
         public string Location { get; set; } = "";
     }
 }
